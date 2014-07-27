c633156aa8d3611fe45d6a7d8d2418c85e14f443
Christian Stade-Schuldt <tafkasorg@yahoo.de>
2014-07-27 22:50:12 +0200

    dashboard values are dynamic

--- solarpi/public/views.py
+++ solarpi/public/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 '''Public section, including homepage and signup.'''
+from datetime import datetime, timedelta
 from flask import (Blueprint, request, render_template)
+from solarpi.data.models import Data
 
 from solarpi.public.forms import LoginForm
 
@@ -9,7 +11,12 @@ blueprint = Blueprint('public', __name__, static_folder="../static")
 
 @blueprint.route("/")
 def home():
-    return render_template("public/home.html")
+    data = Data.query.filter(Data.created_at > (datetime.now() - timedelta(days=1)))
+    actual_energy = data[-1].actual_energy
+    daily_energy = data[-1].daily_energy
+    total_energy = data[-1].total_energy
+    return render_template("public/home.html", actual_energy=actual_energy, daily_energy=daily_energy,
+                           total_energy=total_energy)
 
 
 @blueprint.route("/about/")
