127ce6a14c13f2636a353e24872f8a9da355a95b
Christian Stade-Schuldt <tafkasorg@yahoo.de>
2014-08-20 23:15:24 +0200

    fixed css

--- solarpi/assets.py
+++ solarpi/assets.py
@@ -4,7 +4,7 @@ from flask.ext.assets import Bundle, Environment
 css = Bundle(
     #"libs/bootstrap/dist/css/bootstrap.css",
     #"libs/weather-icons/css/weather-icons.css",
-    ##"css/sb-admin-2.css",
+    "css/sb-admin-2.css",
     filters="cssmin",
     output="public/css/common.css"
 )
@@ -13,7 +13,7 @@ js = Bundle(
     #"libs/jQuery/dist/jquery.js",
     #"libs/bootstrap/dist/js/bootstrap.js",
     #"libs/highcharts/highcharts.js",
-    "libs/metisMenu/metisMenu.js",
+    #"libs/metisMenu/metisMenu.js",
     "js/plugins.js",
     "js/sb-admin-2.js",
     filters='jsmin',
