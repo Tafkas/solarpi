5d18c8891b38d3db84e1d8a19299b0196c647c57
Christian Stade-Schuldt <tafkasorg@yahoo.de>
2014-09-02 09:21:20 +0200

    added one more weather icon

--- solarpi/weather/helper.py
+++ solarpi/weather/helper.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 WEATHER_ICONS = {211: 'wi-storm-showers',
+                 310: 'wi-showers',
                  500: 'wi-showers',
                  501: 'wi-showers',
                  502: 'wi-rain',
@@ -19,5 +20,5 @@ def weather_icon(weather_id):
     """returns the corresponding weather icon for a given weather code from
         http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes"""
     if weather_id not in WEATHER_ICONS:
-        return 'wi-alien' # fallback
+        return 'wi-alien'  # fallback
     return WEATHER_ICONS[weather_id]
